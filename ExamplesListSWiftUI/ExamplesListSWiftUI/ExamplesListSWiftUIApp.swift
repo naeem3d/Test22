8629e780f25201757094287697cc34c040659b67
Naeem <naeem3d@gmail.com>
2023-08-30 10:40:49 -0400

    create new Project

---
+++ ExamplesListSWiftUI/ExamplesListSWiftUI/ExamplesListSWiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamplesListSWiftUIApp.swift
+//  ExamplesListSWiftUI
+//
+//  Created by naeem alabboodi on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct ExamplesListSWiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
