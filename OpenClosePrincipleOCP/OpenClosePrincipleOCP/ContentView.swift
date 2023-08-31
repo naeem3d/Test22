e31bc55c70049d3d9372db3bd32e594385c5e07a
Naeem <naeem3d@gmail.com>
2023-08-31 10:46:39 -0400

    create new project explain the OCP(open close Principle and in In this example, we have a ButtonView that takes a ButtonViewDelegate. The ContentView extends the delegate and implements buttonTapped() without modifying the original ButtonView. It keeps the ButtonView closed for modification and open for extension. in this example : we can using protocols and extensions to add new behavior to existing views without modifying their original code.

---
+++ OpenClosePrincipleOCP/OpenClosePrincipleOCP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OpenClosePrincipleOCP
+//
+//  Created by naeem alabboodi on 8/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OpenClosePrincipleOCP/OpenClosePrincipleOCP/OpenClosePrincipleOCPApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenClosePrincipleOCPApp.swift
+//  OpenClosePrincipleOCP
+//
+//  Created by naeem alabboodi on 8/31/23.
+//
+
+import SwiftUI
+
+@main
+struct OpenClosePrincipleOCPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
